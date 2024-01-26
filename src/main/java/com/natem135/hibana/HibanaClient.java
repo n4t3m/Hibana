10ea0a3eadb8ab5d04574840b33f3291feafa615
NateM135 <melwaninate@gmail.com>
2024-01-26 14:32:34 -0800

    poc to render enabled modules

--- src/main/java/com/natem135/hibana/HibanaClient.java
+++ src/main/java/com/natem135/hibana/HibanaClient.java
@@ -4,6 +4,9 @@ import com.natem135.hibana.modules.*;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.DrawContext;
 
 import java.util.Arrays;
 import java.util.List;
@@ -17,7 +20,7 @@ public class HibanaClient implements ClientModInitializer {
     public  static final AutoRespawnModule autoRespawnModule = new AutoRespawnModule();
     public  static final AutoFishEntityModule autoFishEntityModule = new AutoFishEntityModule();
 
-    List<ToggleableModule> mods = Arrays.asList(
+    public static List<ToggleableModule> mods = Arrays.asList(
             boatFlyModule,
             xrayModule,
             playerFlyModule,
@@ -34,5 +37,22 @@ public class HibanaClient implements ClientModInitializer {
             KeyBindingHelper.registerKeyBinding(mod.keybind);
             ClientTickEvents.END_CLIENT_TICK.register(mod::tick);
         }
+        HudRenderCallback.EVENT.register(HibanaClient::render);
+
     }
+
+    private static void render(DrawContext drawContext, float v) {
+        MinecraftClient client = MinecraftClient.getInstance();
+        // drawContext.drawText(client.textRenderer, "\"§cT§6e§6s§2t§9W§do§5rd\"", 0, 10, 16777215, true);
+        int enabled_modules = 0;
+        for(ToggleableModule module : HibanaClient.mods) {
+            if(module.module_enabled) {
+                drawContext.drawText(client.textRenderer, module.module_name, 0, 10+(enabled_modules*10), 16777215, true);
+                enabled_modules++;
+            }
+        }
+        // drawContext.drawText(client.textRenderer, String.format("§1§3§4§5§6§7§2%s§r", "NATEM135"), 100, 100, 16777215, true);
+
+    }
+
 }
