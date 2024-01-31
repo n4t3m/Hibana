6fb1d93967ff24536f307ff79782c031635e90c1
NateM135 <melwaninate@gmail.com>
2024-01-30 18:20:26 -0800

    clickgui category tab is now drag-able

--- src/main/java/com/natem135/hibana/gui/ClickGUI.java
+++ src/main/java/com/natem135/hibana/gui/ClickGUI.java
@@ -1,7 +1,7 @@
 package com.natem135.hibana.gui;
 
 import com.natem135.hibana.Hibana;
-import com.natem135.hibana.gui.screen.Frame;
+import com.natem135.hibana.gui.screen.CategoryFrame;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.text.Text;
@@ -11,20 +11,20 @@ import java.util.ArrayList;
 public class ClickGUI extends Screen {
 
     public static final ClickGUI CLICK_GUI_INSTANCE = new ClickGUI();
-    private final ArrayList<Frame> frames = new ArrayList<>();
+    private final ArrayList<CategoryFrame> categoryFrames = new ArrayList<>();
 
 
     public ClickGUI() {
         super(Text.of("Click GUI"));
-        frames.add(
-                new Frame(100, 100, 100, 100, "Demo")
+        categoryFrames.add(
+                new CategoryFrame(100, 100, 100, 20, "Demo")
         );
     }
 
     // DrawScreen equivalent
     @Override
     public void render(DrawContext context, int mouseX, int mouseY, float delta) {
-        for(Frame frame : frames) {
+        for(CategoryFrame frame : categoryFrames) {
             frame.onRender(context, mouseX, mouseY, delta);
         }
         super.render(context, mouseX, mouseY, delta);
@@ -33,7 +33,7 @@ public class ClickGUI extends Screen {
     @Override
     public boolean mouseClicked(double mouseX, double mouseY, int button) {
         Hibana.LOGGER.info("mouseClicked in ClickGUI Class");
-        for(Frame frame : frames) {
+        for(CategoryFrame frame : categoryFrames) {
             frame.mouseClicked(mouseX, mouseY, button);
         }
         return super.mouseClicked(mouseX, mouseY, button);
@@ -41,6 +41,9 @@ public class ClickGUI extends Screen {
 
     @Override
     public boolean mouseReleased(double mouseX, double mouseY, int button) {
+        for(CategoryFrame frame : categoryFrames) {
+            frame.mouseReleased(mouseX, mouseY, button);
+        }
         return super.mouseReleased(mouseX, mouseY, button);
     }
 
---
+++ src/main/java/com/natem135/hibana/gui/screen/CategoryFrame.java
@@ -0,0 +1,86 @@
+package com.natem135.hibana.gui.screen;
+
+import com.natem135.hibana.Hibana;
+import com.natem135.hibana.modules.ModuleManager;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.DrawContext;
+
+import java.awt.*;
+import java.util.ArrayList;
+
+public class CategoryFrame {
+    public  int x, y, width, height, dragXOffset, dragYOffset;
+    public String category;
+    public boolean drag, extend;
+    public ArrayList<ModuleButton>  buttons = new ArrayList<>();
+    public MinecraftClient client = MinecraftClient.getInstance();
+
+
+    public CategoryFrame(int x, int y, int width, int height, String category) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.category = category;
+
+        this.drag = false;
+        this.extend = false;
+
+        buttons.add(
+                new ModuleButton(
+                        this,
+                        ModuleManager.xrayModule,
+                        0
+                )
+        );
+        buttons.add(
+                new ModuleButton(
+                        this,
+                        ModuleManager.autoRespawnModule,
+                        1
+                )
+        );
+    }
+
+    public void onRender(DrawContext context, int mouseX, int mouseY, float delta) {
+        context.drawBorder(x, y, width, height, Color.green.getRGB());
+        context.fill(x+1,y+1, x+width-1, y+height-1, Color.black.getRGB());
+        if(extend) {
+            for(ModuleButton button : buttons) {
+                button.onRender(context, mouseX, mouseY, delta);
+            }
+        }
+    }
+
+    public boolean isHovered(double mouseX, double mouseY){
+        return mouseX > x && mouseX < x + width && mouseY > y && mouseY < y + height;
+    }
+
+    public void mouseClicked(double mouseX, double mouseY, int button) {
+        if(isHovered(mouseX , mouseY)) {
+            if(button==0) {
+                Hibana.LOGGER.info("MOUSE CLICKED AND HOVERED");
+                extend = !extend;
+                Hibana.LOGGER.info(String.format("Extent set to: %b", extend));
+            }
+            else {
+                Hibana.LOGGER.info(String.format("Starting drag of Component at Component x %d y %d and Mouse X %.2f Y %.2f", x, y, mouseX, mouseY));
+                drag = true;
+                dragXOffset = (int) mouseX - this.x;
+                dragYOffset = (int) mouseY - this.y;
+            }
+        }
+    }
+
+    public void mouseReleased(double mouseX, double mouseY, int button) {
+        if(button==1) {
+            Hibana.LOGGER.info(String.format("Finished Dragging. X: %.2f, Y: %.2f", mouseX, mouseY));
+            drag = false;
+            x = (int) mouseX - dragXOffset;
+            y = (int) mouseY - dragYOffset;
+        }
+    }
+
+
+}
+
